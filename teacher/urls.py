1ae23d9450e6cdb633f6237058e58baa3b4e00a1
nguyenthetai123 <nguyenthetai.itdev97@gmail.com>
2023-12-04 20:27:58 +0700

    class baseview API crud

--- teacher/urls.py
+++ teacher/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('', views.hello_word),
-    path('teach_api/<int:pk>',views.tech_api)
+    path('teach_api/<int:pk>',views.tech_api),
+    path('techerView',views.teacherView.as_view()),
+    path('tech_detail/<int:pk>',views.TechDetail.as_view())
 
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .serializers import TeacherSerializer
 from .models import Teach
 from rest_framework import status
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -12,7 +13,7 @@ from rest_framework import status
 def hello_word(request):
     if request.method == 'GET':
         stu = Teach.objects.all()
-        serializer = TeacherSerializer(instance=stu,many=True)
+        serializer = TeacherSerializer(instance=stu, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     if request.method == 'POST':
         serializer = TeacherSerializer(data=request.data)
@@ -33,8 +34,42 @@ def tech_api(requets, pk):
                 serializer.save()
                 return Response(data=serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if requets.method=='DELETE':
+    if requets.method == 'DELETE':
         if tech:
             tech.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         pass
+
+
+class teacherView(APIView):
+    def get(self, request):
+        tech = Teach.objects.all()
+        serializer = TeacherSerializer(tech, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = TeacherSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TechDetail(APIView):
+    def get(self, request, pk):
+        tech = get_object_or_404(Teach, pk=pk)
+        serializer = TeacherSerializer(tech)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, requets, pk):
+        tech = get_object_or_404(Teach, pk=pk)
+        serializer = TeacherSerializer(tech, data=requets.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        tech = Teach.objects.get(pk=pk)
+        tech.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
