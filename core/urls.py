c357cb3b215d01fa283a9c4e6efc2472bc45286d
nguyenthetai123 <nguyenthetai.itdev97@gmail.com>
2023-12-25 22:23:31 +0700

    Ordering Filter in Django REST Framework

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 router = DefaultRouter()
 router.register('core',views.StudentView,basename='studentView')
+router.register('core2',views.StudentView1,basename='studentView1')
 urlpatterns=[
     path('', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets,generics,status
 from rest_framework.permissions import IsAdminUser
 from rest_framework.permissions import IsAuthenticated
 from .permission import MyPermission
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 # Create your views here.
 
 
@@ -16,11 +16,18 @@ class StudentView(viewsets.ModelViewSet):
     authentication_classes = [SessionAuthentication]
     permission_classes = [MyPermission]
     filter_backends = [SearchFilter]
-    # search_fields=['city']
+    # search_fields=['city'] http://127.0.0.1:8000/core/core/?search=asdsad
     # search_fields = ['city', 'name']
     search_fields = ['^city', ]
     pass
+class StudentView1(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [MyPermission]
+    filter_backends = [OrderingFilter]
 
+    pass
 #de custom permision
 #has_permission(self,request,view)
 #has_object_permission(self,request,view,obj)
